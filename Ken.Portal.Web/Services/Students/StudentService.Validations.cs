d25ada417f787695114437840d97a8c2b9263e90
Ken Ramos <kenneth.ramos@avanade.com>
2021-02-19 16:19:12 -0800

    ShouldThrowValidationExceptionOnRegisterIfBirthDateIsInvalidAndLogItAsync -> PASS

--- Ken.Portal.Web/Services/Students/StudentService.Validations.cs
+++ Ken.Portal.Web/Services/Students/StudentService.Validations.cs
@@ -33,6 +33,11 @@ namespace Ken.Portal.Web.Services.Students
                         parameterName: nameof(Student.FirstName),
                         parameterValue: student.FirstName);
 
+                case { } when IsInvalid(student.BirthDate):
+                    throw new InvalidStudentException(
+                        parameterName: nameof(Student.BirthDate),
+                        parameterValue: student.BirthDate);
+
                 case { } when IsInvalid(student.CreatedBy):
                     throw new InvalidStudentException(
                         parameterName: nameof(Student.CreatedBy),
@@ -47,5 +52,6 @@ namespace Ken.Portal.Web.Services.Students
 
         private static bool IsInvalid(Guid id) => id == Guid.Empty;
         private static bool IsInvalid(string text) => string.IsNullOrWhiteSpace(text);
+        private static bool IsInvalid(DateTimeOffset date) => date == default;
     }
 }
