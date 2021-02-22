c2eb068696d7df5b098f63133cc6ca129054eb30
Ken Ramos <kenneth.ramos@avanade.com>
2021-02-22 14:11:02 -0800

    ShouldInitializeComponent -> PASS

---
+++ Ken.Portal.Web.Tests.Unit/Views/StudentRegistrationComponents/StudentRegistrationComponentTests.Render.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using Ken.Portal.Web.Models.ContainerComponents;
+using Ken.Portal.Web.Views.Components;
+using Xunit;
+
+namespace Ken.Portal.Web.Tests.Unit.Views.StudentRegistrationComponents
+{
+    public partial class StudentRegistrationComponentTests
+    {
+        [Fact]
+        public void ShouldInitializeComponent()
+        {
+            // given
+            ComponentState expectedComponentState = ComponentState.Loading;
+
+            // when
+            var initialStudentRegistrationComponent = new StudentRegistrationComponent();
+
+            // then
+            initialStudentRegistrationComponent.State.Should().Be(expectedComponentState);
+            initialStudentRegistrationComponent.Exception.Should().BeNull();
+            initialStudentRegistrationComponent.StudentIdentityTextBox.Should().BeNull();
+            initialStudentRegistrationComponent.StudentFirstNameTextBox.Should().BeNull();
+            initialStudentRegistrationComponent.StudentMiddleNameTextBox.Should().BeNull();
+            initialStudentRegistrationComponent.StudentLastNameTextBox.Should().BeNull();
+            initialStudentRegistrationComponent.StudentView.Should().BeNull();
+        }
+    }
+}
---
+++ Ken.Portal.Web.Tests.Unit/Views/StudentRegistrationComponents/StudentRegistrationComponentTests.cs
@@ -0,0 +1,21 @@
+﻿using Bunit;
+using Ken.Portal.Web.Services.StudentViews;
+using Ken.Portal.Web.Views.Components;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+
+namespace Ken.Portal.Web.Tests.Unit.Views.StudentRegistrationComponents
+{
+    public partial class StudentRegistrationComponentTests : TestContext
+    {
+        private readonly Mock<IStudentViewService> studentViewServiceMock;
+        private IRenderedComponent<StudentRegistrationComponent> renderedStudentRegistrationComponent;
+
+        public StudentRegistrationComponentTests()
+        {
+            this.studentViewServiceMock = new Mock<IStudentViewService>();
+            this.Services.AddScoped(services => this.studentViewServiceMock.Object);
+            this.Services.AddServerSideBlazor();
+        }
+    }
+}
