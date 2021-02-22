8b78b0efcbd2198ce7c28dd983a4fce4d2f5ab70
Ken Ramos <kenneth.ramos@avanade.com>
2021-02-22 15:47:06 -0800

    ShouldRenderComponent -> FAIL

--- Ken.Portal.Web.Tests.Unit/Views/StudentRegistrationComponents/StudentRegistrationComponentTests.Render.cs
+++ Ken.Portal.Web.Tests.Unit/Views/StudentRegistrationComponents/StudentRegistrationComponentTests.Render.cs
@@ -71,11 +71,14 @@ namespace Ken.Portal.Web.Tests.Unit.Views.StudentRegistrationComponents
             this.renderedStudentRegistrationComponent.Instance.StudentLastNameTextBox.Placeholder
                 .Should().Be(expectedLastNameTextBoxPlaceholder);
 
-            this.renderedStudentRegistrationComponent.Instance.SubmitButton
+            this.renderedStudentRegistrationComponent.Instance.StudentGenderDropDown
                 .Should().NotBeNull();
 
             this.renderedStudentRegistrationComponent.Instance.SubmitButton.Label
                 .Should().Be(expectedSubmitButtonLabel);
+
+            this.renderedStudentRegistrationComponent.Instance.SubmitButton
+                .Should().NotBeNull();
 
             this.renderedStudentRegistrationComponent.Instance.StudentView.Should().BeNull();
             this.renderedStudentRegistrationComponent.Instance.Exception.Should().BeNull();
