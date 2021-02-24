c29974a2ac3013391057a805d45bf5aab9ee0028
Ken Ramos <kenneth.ramos@avanade.com>
2021-02-23 16:08:17 -0800

    ShouldRenderComponent -> PASS

--- Ken.Portal.Web.Tests.Unit/Views/StudentRegistrationComponents/StudentRegistrationComponentTests.Render.cs
+++ Ken.Portal.Web.Tests.Unit/Views/StudentRegistrationComponents/StudentRegistrationComponentTests.Render.cs
@@ -60,11 +60,17 @@ namespace Ken.Portal.Web.Tests.Unit.Views.StudentRegistrationComponents
             this.renderedStudentRegistrationComponent.Instance.StudentIdentityTextBox
                 .Should().NotBeNull();
 
+            this.renderedStudentRegistrationComponent.Instance.StudentIdentityTextBox.IsDisabled
+                .Should().BeFalse();
+
             this.renderedStudentRegistrationComponent.Instance.StudentIdentityTextBox.Placeholder
                 .Should().Be(expectedIdentityTextBoxPlaceholder);
 
             this.renderedStudentRegistrationComponent.Instance.StudentFirstNameTextBox
                 .Should().NotBeNull();
+
+            this.renderedStudentRegistrationComponent.Instance.StudentFirstNameTextBox.IsDisabled
+                .Should().BeFalse();
 
             this.renderedStudentRegistrationComponent.Instance.StudentFirstNameTextBox.Placeholder
                 .Should().Be(expectedFirstNameTextBoxPlaceholder);
@@ -72,11 +78,17 @@ namespace Ken.Portal.Web.Tests.Unit.Views.StudentRegistrationComponents
             this.renderedStudentRegistrationComponent.Instance.StudentMiddleNameTextBox
                 .Should().NotBeNull();
 
+            this.renderedStudentRegistrationComponent.Instance.StudentMiddleNameTextBox.IsDisabled
+                .Should().BeFalse();
+
             this.renderedStudentRegistrationComponent.Instance.StudentMiddleNameTextBox.Placeholder
                 .Should().Be(expectedMiddleNameTextBoxPlaceholder);
 
             this.renderedStudentRegistrationComponent.Instance.StudentLastNameTextBox
                 .Should().NotBeNull();
+
+            this.renderedStudentRegistrationComponent.Instance.StudentLastNameTextBox.IsDisabled
+                .Should().BeFalse();
 
             this.renderedStudentRegistrationComponent.Instance.StudentLastNameTextBox.Placeholder
                 .Should().Be(expectedLastNameTextBoxPlaceholder);
@@ -84,14 +96,23 @@ namespace Ken.Portal.Web.Tests.Unit.Views.StudentRegistrationComponents
             this.renderedStudentRegistrationComponent.Instance.StudentGenderDropDown
                 .Should().NotBeNull();
 
+            this.renderedStudentRegistrationComponent.Instance.StudentGenderDropDown.IsDisabled
+                .Should().BeFalse();
+
             this.renderedStudentRegistrationComponent.Instance.StudentGenderDropDown.Value
                 .Should().BeOfType(typeof(StudentViewGender));
 
             this.renderedStudentRegistrationComponent.Instance.DateOfBirthPicker
                 .Should().NotBeNull();
 
+            this.renderedStudentRegistrationComponent.Instance.DateOfBirthPicker.IsDisabled
+                .Should().BeFalse();
+
             this.renderedStudentRegistrationComponent.Instance.SubmitButton
                 .Should().NotBeNull();
+
+            this.renderedStudentRegistrationComponent.Instance.SubmitButton.IsDisabled
+                .Should().BeFalse();
 
             this.renderedStudentRegistrationComponent.Instance.SubmitButton.Label
                 .Should().Be(expectedSubmitButtonLabel);
@@ -115,7 +136,7 @@ namespace Ken.Portal.Web.Tests.Unit.Views.StudentRegistrationComponents
             this.studentViewServiceMock.Setup(service =>
                 service.AddStudentViewAsync(It.IsAny<StudentView>()))
                     .ReturnsAsync(
-                        value: someStudentView, 
+                        value: someStudentView,
                         delay: TimeSpan.FromMilliseconds(500));
 
             // when
